3f088803b95d63d26083da8df27bbeb0a23a8538
Anthony Kulis <anthony.kulis@gmail.com>
2016-08-23 09:48:23 -0500

    lab assignment

---
+++ labs/generic_box/Box.java
@@ -0,0 +1,57 @@
+public class Box<T> {
+  private T _item;
+  private String _description;
+
+  public Box(){
+    this._item = null;
+    this._description = null;
+  }
+
+  public Box add(T item){
+    this._item = item;
+    return this;
+  }
+
+  public T remove(){
+    T item = this._item;
+    this._item = null;
+    return item;
+  }
+
+  public Box setDescription(String description){
+    this._description = description;
+    return this;
+  }
+
+  public String getDescription(){
+    return this._description;
+  }
+
+  public static void main(String args[]){
+    Box b = new Box();
+    b.setDescription("My awesome test box");
+
+    System.out.println("Box description: " + b.getDescription());
+    
+    if(!b.getDescription().equals("My awesome test box")){
+      throw new RuntimeException("Description doesnt match");
+    }
+
+    String item = "Pretty Item";
+    b.add(item);
+    String removedItem = (String)b.remove();
+
+    System.out.println("removed item: " + item);
+
+    if(!removedItem.equals("Pretty Item")){
+      throw new RuntimeException("What I removed is not what I put in");
+    }
+
+    String removedAgain = (String)b.remove();
+    if(removedAgain != null){
+      throw new RuntimeException("Whoops, I removed something else");
+    }
+
+    System.out.println("Tests completed successully");
+  }
+}
---
+++ labs/generic_box/README.md
@@ -0,0 +1,18 @@
+# Generic Box Lab
+As business came to us and asked us to create a box that can hold one of its objects for resell. For some reason they think putting an object in a pretty digital box will help drive sales and have people use their object. I tried to tell this this was irrational but they wanted it anyways.
+
+## Requirements
+
+1. The box must hold one item. Since it is meant to be reusable for any object, it has to be generic.
+2. The box must be pretty, so it must take a description so users who wish to download the box can be sold on how great the object inside is.
+3. The business must be able to put the object in the box.
+4. The user must be able to remove the object from the box.
+5. The user may only remove the item *once*
+6. This box will never be extended nor used anywhere else, so a Box class adequate.
+7. Write your unit test to check for adding, removing, setting the description, and reading the description.
+
+## Due Date
+* At the end of lab 8/23/2016.
+
+## Grading
+* This will not be graded.
