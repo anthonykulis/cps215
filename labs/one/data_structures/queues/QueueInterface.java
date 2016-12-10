908f3a62d09e7651a76568ba39abb48054148486
Anthony <anthony.kulis@gmail.com>
2016-12-10 10:41:25 -0600

    clean up for semester

--- labs/one/data_structures/queues/QueueInterface.java
+++
@@ -1,9 +0,0 @@
-package labs.one.data_structures.queues;
-
-public interface QueueInterface<T> {
-  public void push(T item);
-  public T shift();
-  public T peek();
-  public boolean isEmpty();
-  public void clear();
-}
